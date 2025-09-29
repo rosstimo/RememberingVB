868f256704cd3ec8f63dcd650411e545b156cda9
rosstimo <rosstimo@isu.edu>
2025-09-29 09:43:14 -0600

    add shave and a hair cut, two bits...

--- RememberingVB/RememberingVB.vb
+++ RememberingVB/RememberingVB.vb
@@ -1,7 +1,31 @@
 ﻿Module RememberingVB
 
     Sub Main()
+        Console.WriteLine("Hello, World!")
+        'Console.Beep(75, 500)
+        'play notes
 
+        'C
+        Console.Beep(262, 500)
+        'G
+        Console.Beep(196, 250)
+        'Gb
+        Console.Beep(185, 250)
+        'G#
+        Console.Beep(208, 500)
+        'G
+        Console.Beep(196, 500)
+
+        'silent rest
+        System.Threading.Thread.Sleep(500)
+
+        'B
+        Console.Beep(247, 500)
+        'C
+        Console.Beep(262, 500)
+
+
+        Console.Read()
     End Sub
 
 End Module
